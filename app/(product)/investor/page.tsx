025172d4ed5138095ee94891865bc184af059c21
Ntare22 <jim.ntare@gmail.com>
2023-05-25 18:17:17 +0200

    update mobile version investor page

--- app/(product)/investor/page.tsx
+++ app/(product)/investor/page.tsx
@@ -24,7 +24,7 @@ export default function Investor() {
           <div className=''>
             <h1 className='font-medium text-3xl'>Track your investments grow your wealth</h1>
             <div className='lg:flex'>
-              <div className='py-7 bg-red-500'>
+              <div className='py-7'>
                 <div className='flex items-center py-2'>
                   <div className='w-1/6'>
                     <Image src='/click.svg' alt='click' width={20} height={10}  />
@@ -58,7 +58,7 @@ export default function Investor() {
               </div>
             </div>
           </div>
-          {/* <div className='pt-[30rem] lg:pt-0'>
+          <div className='pt-[30rem] lg:pt-0'>
             <h1 className='font-medium text-3xl'>Track your investments grow your wealth</h1>
             <div className='lg:flex'>
               <div className='py-7'>
@@ -94,7 +94,7 @@ export default function Investor() {
                 <PictureCard picUrl='/african-woman.jpg' width={250} height={10} />
               </div>
             </div>
-          </div> */}
+          </div>
         </div>
         <section className='pt-[25rem] mx-4 lg:pt-16 lg:mx-[10rem] xl:mx-[15rem]'>
             <Faq />
